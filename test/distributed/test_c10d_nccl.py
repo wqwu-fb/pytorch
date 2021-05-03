ffde76e138d39bb4c12e3802e050ca61302684fd
Rohan Varma <rvarm1@fb.com>
2021-05-03 11:20:08 -0700

    [wip] enhance DDPSink to work for general outputs
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57073
    
    Enhances use of DDPSink to work for all output types DDP supports as per https://github.com/pytorch/pytorch/issues/55876.
    
    TODO: Add additional testing for tuple, list, dict return types
    ghstack-source-id: 127976411
    
    Differential Revision: [D27756985](https://our.internmc.facebook.com/intern/diff/D27756985/)

--- test/distributed/test_c10d_nccl.py
+++ test/distributed/test_c10d_nccl.py
@@ -1841,6 +1841,7 @@ class DistributedDataParallelTest(test_c10d_common.AbstractDistributedDataParall
         else:
             output = model(input_var)
         l = loss(output, target)
+        print(f"Got l {l}")
         l.backward()
 
     def _test_ddp_checkpointing(
@@ -1879,6 +1880,7 @@ class DistributedDataParallelTest(test_c10d_common.AbstractDistributedDataParall
                 self.assertTrue(i.grad is not None)
                 self.assertTrue(j.grad is not None)
                 self.assertEqual(i.grad, j.grad)
+                # print(f"grads same {i.grad}, {j.grad}")
 
     # DDP works as expect when layer is checkpointed only once
     @requires_nccl()
