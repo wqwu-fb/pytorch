95aa818b8dcf477ea509246dc681bb6b934a3901
Lillian Johnson <lillianjohnson@fb.com>
2021-05-11 09:31:56 -0700

    [torch.package/TorchScript] flag to gate allowance of TS serializaiton in torch.package
    ghstack-source-id: 5320bf8387228f2825565f4b3ca0ee7b76057b23
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57678

--- torch/package/package_exporter.py
+++ torch/package/package_exporter.py
@@ -30,6 +30,7 @@ from .find_file_dependencies import find_files_source_depends_on
 from .glob_group import GlobGroup, GlobPattern
 from .importer import Importer, OrderedImporter, sys_importer
 
+_gate_torchscript_serialization = True
 
 class EmptyMatchError(Exception):
     """This is an exception that is thrown when a mock or extern is marked as
@@ -516,6 +517,13 @@ node [shape=box];
             return ("storage", storage_type, obj_key, location, obj.size())
 
         if hasattr(obj, "__reduce_package__"):
+            if _gate_torchscript_serialization and isinstance(
+                obj, torch.jit.RecursiveScriptModule
+            ):
+                raise Exception(
+                    "Serializing ScriptModules directly into a package is a beta feature. "
+                    "To use, set `PackageExporter.gate_torchscript_serialization` to `False`."
+                )
             if self.serialized_reduces.get(id(obj)) is None:
                 self.serialized_reduces[id(obj)] = ("reduce_package", id(obj), *obj.__reduce_package__(self))
 
